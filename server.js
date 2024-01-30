504ba8490b0a990aba21509f6d376ab1ffabff4c
devsrinione <srinivasan.s@dsmsoft.net>
2024-01-30 16:43:59 +0530

    added ua

--- server.js
+++ server.js
@@ -295,5 +295,40 @@ app.post("/autoCompleteData", async (req, res) => {
     }
     reqData();
   });
+  app.post("/loginUserAuth", async (req, res) => {
+    const { filterdata, requst } = req.body;
+  
+    const url = process.env.BASE_URL + requst;
+    console.log(url);
+    async function reqData() {
+      await axios
+        .post(url, filterdata, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          responseType: "stream",
+          httpsAgent: new Agent({ rejectUnauthorized: false }),
+        })
+        .then(function (response) {
+          let data = "";
+          response.data.on("data", (chunk) => {
+            data += chunk.toString();
+          });
+          response.data.on("end", () => {
+            const stringResponse = JSON.stringify(data);
+            if (stringResponse.match("html") || data === undefined) {
+              res.status(400).json({
+                message: "server response is unknown",
+              });
+            } else {
+              const jsonData = JSON.parse(data);
+              res.status(200).send(jsonData);
+            }
+          });
+        });
+    }
+    reqData();
+  });
 // export the app for vercel serverless functions
 module.exports = app;
