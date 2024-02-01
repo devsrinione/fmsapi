a7cd623bf570f764163f513cb525d777ef086630
devsrinione <140168220+devsrinione@users.noreply.github.com>
2024-02-01 17:15:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -508,7 +508,7 @@ app.post("/autoCompleteData", async (req, res) => {
  app.post("/menus", async (req, res) => {
     const { filterdata, requst } = req.body;
   
-    const url = process.env.R_B_U + requst +`?${filterdata}`
+    const url = process.env.BASE_URL + requst +`?${filterdata}`
     console.log(url);
     async function reqData() {
       await axios
